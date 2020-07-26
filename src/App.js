612586b56bddc038921f409327c4a309634d6f1f
Vish Singh <vishalicious213@gmail.com>
2020-07-26 16:32:27 -0700

    wrapped App in AuctionsProvider

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import AuctionGallery from './components/AuctionGallery';
 import DisplayAuctionItem from './components/DisplayAuctionItem';
+import AuctionsProvider from './components/AuctionsContext';
 
 function App() {
   return (
-    <div>
-      <Route exact path='/' component={AuctionGallery} />
-      {/* <Route path='/item/:itemID' component={DisplayAuctionItem} /> */}
-      <Route path='/item/:itemID'>
-        <DisplayAuctionItem />
-      </Route>
-    </div>
+    <AuctionsProvider>
+      <div>
+        <Route exact path='/' component={AuctionGallery} />
+        {/* <Route path='/item/:itemID' component={DisplayAuctionItem} /> */}
+        <Route path='/item/:itemID'>
+          <DisplayAuctionItem />
+        </Route>
+      </div>
+    </AuctionsProvider>
   );
 }
 
