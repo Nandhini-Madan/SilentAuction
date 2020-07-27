7e2851fb6613146c99b65ec89de333d7ccc18e46
Vish Singh <vishalicious213@gmail.com>
2020-07-27 15:16:32 -0700

    initial format for DisplayAuctionItem

--- src/components/DisplayAuctionItem.js
+++ src/components/DisplayAuctionItem.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { AuctionsContext } from './AuctionsContext';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 const DisplayAuctionItem = () => {
     const [itemsArray, setItemsArray] = useContext(AuctionsContext);
@@ -19,17 +20,44 @@ const DisplayAuctionItem = () => {
     )
 
     return (
-        <section>
-            <div>Display Auction Item #{params.itemID}</div>
+        <Section>
             {auctionItem && 
-                <section>
+                <section className='item-container'>
+                    <div className='name'>Auction Item #{params.itemID}</div>
                     <img src={auctionItem.avatar} alt={auctionItem.email}></img>
-                    <div>name: {auctionItem.first_name} {auctionItem.last_name}</div>
-                    <div>email: {auctionItem.email}</div>
+                    <div>Description: {auctionItem.first_name} {auctionItem.last_name}</div>
+                    <div>Starting Price: {auctionItem.email}</div>
                 </section>
             }
-        </section>
+        </Section>
     )
 }
 
+const Section = styled.section`
+    border: 1px solid red;
+    padding: 1rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    .item-container {
+        border: 1px solid orange;
+        padding: .5rem;
+        width: clamp(15rem, 50%, 30rem);
+        diplay: flex;
+        flex-direction: column;
+        position: relative;
+
+        .name {
+            font-size: 2rem;
+            text-align: center;
+        }
+
+        img {
+            border-top-left-radius: 1rem;
+            border-top-right-radius: 1rem;
+            width: 100%;
+        }
+    }
+`
 export default DisplayAuctionItem;
