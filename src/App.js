96040d25b3210cb2be08eba4d5e18c2f52b75467
Vish Singh <vishalicious213@gmail.com>
2020-07-27 21:42:37 -0700

    cleaned up App.js, changed app name in index.html to match Jacob's title

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import React from "react";
-import {Link,Route,Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import Header from './components/header';
 import Registeration from './components/Registeration';
 import Login from "./components/Login";
 import { AuctionsProvider } from './components/AuctionsContext';
 import AuctionGallery from './components/AuctionGallery';
 import DisplayAuctionItem from './components/DisplayAuctionItem';
-import Header from './components/header';
-import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <AuctionsProvider>
       <div className='container'>
+        <Header />
         <Route exact path='/' component={AuctionGallery} />
-        {/* <Route path='/item/:itemID' component={DisplayAuctionItem} /> */}
-        <Route path='/item/:itemID'>
-          <DisplayAuctionItem />
-        </Route>
-        <Header/>
-      <Switch>
-        <Route path="/login">
-          <Login/>
-        </Route>
-        <Route path='/Register'>
-	        <Registeration/>
-	      </Route>
+        <Route path='/item/:itemID' component={DisplayAuctionItem} />
 
-      </Switch>
-    </div>
+        <Switch>
+          <Route path="/login">
+            <Login/>
+          </Route>
+          <Route path='/Register'>
+            <Registeration/>
+          </Route>
+        </Switch>
+      </div>
     </AuctionsProvider>
   );
 }
