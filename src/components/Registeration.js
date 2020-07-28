cdd50435b61ab0a3fc97b362fad101712f7566a6
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-07-28 14:19:19 -0700

    fixed unexpected whitespace in registration.js

--- src/components/Registeration.js
+++ src/components/Registeration.js
@@ -17,7 +17,7 @@ const Registeration = props => {
         UserType: yup.string().notRequired(),
         username: yup.string().required("Please Enter Your Name").min(2, "This is not your real name"),
         email: yup.string().email().required("Please Enter email"),
-        password: yup.string().required("Please enter a password") .matches(
+        password: yup.string().required("Please enter a password").matches(
             /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
             "Password must contain at least 8 characters, one uppercase, one number and one special case character"
           ),
