4696fd973d5146f6e656a65ffc96d079bd495886
Vish Singh <vishalicious213@gmail.com>
2020-07-27 23:52:44 -0700

    DisplayAuctionItem: added modify & delete buttons, renamed auction list button, styled component

--- src/components/DisplayAuctionItem.js
+++ src/components/DisplayAuctionItem.js
@@ -21,10 +21,15 @@ const DisplayAuctionItem = () => {
                 <section className='item-container'>
                     <div className='name'>Auction Item #{params.itemID}</div>
                     <img src={auctionItem.avatar} alt={auctionItem.email}></img>
-                    <div>Description: {auctionItem.first_name} {auctionItem.last_name}</div>
-                    <div>Starting Price: {auctionItem.email}</div>
+                    <section className='detail-container'>
+                        <div className='details'><b>Description:</b></div>
+                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        <div className='details'><b>Starting Price:</b> {auctionItem.email}</div>
+                    </section>
                     <div className='button-holder'>
-                        <Link to={`/auctions`}><button>Return to auction list</button></Link>
+                        <Link to={`/auctions`}><button>Modify Auction</button></Link>
+                        <Link to={`/auctions`}><button>Delete Auction</button></Link>
+                        <Link to={`/auctions`}><button>Auction List</button></Link>
                     </div>
                 </section>
             }
@@ -32,8 +37,11 @@ const DisplayAuctionItem = () => {
     )
 }
 
+const background1 = '#C99946';
+const buttonText2 = '#EDEBE1';
+const background2 = '#C05934';
 const Section = styled.section`
-    border: 1px solid red;
+    // border: 1px solid red;
     padding: 1rem;
     display: flex;
     flex-wrap: wrap;
@@ -43,7 +51,7 @@ const Section = styled.section`
     color: #EDEBE1;
 
     .item-container {
-        border: 1px solid orange;
+        // border: 1px solid orange;
         padding: .5rem;
         width: clamp(25rem, 50%, 50rem);
         diplay: flex;
@@ -52,27 +60,51 @@ const Section = styled.section`
         .name {
             font-size: 2.25rem;
             text-align: center;
+            color: white;
+            margin-bottom: 1rem;
         }
 
         img {
             border-top-left-radius: 1rem;
             border-top-right-radius: 1rem;
             width: 100%;
+            border-top: 1px solid ${buttonText2};
+            border-right: 1px solid ${buttonText2};
+            border-left: 1px solid ${buttonText2};
+        }
+
+        .detail-container {
+            background: #A59E69;
+            border-radius: 1rem;
+            border: 1px solid #34362F;
+            padding: 1rem;
+        }
+
+        .details {
+            margin-top: 1rem;
+
+            b {
+                font-weight: 700;
+                color: white;
+            }
         }
 
         .button-holder {
             display: flex;
-            justify-content: center;
+            justify-content: space-evenly;
+            margin-top: 1rem;
+            margin-bottom: 3rem;
         }
 
         button {
-            width: 16rem;
+            width: 12rem;
             margin-top: 1rem;
             border-radius: 1rem;
             font-size: 1.25rem;
+            background: ${background1};
             :hover {
-                background: black;
-                color: white;
+                background: ${background2};
+                color: ${buttonText2};
             }
         }
     }
