32ac725614aa20859a6c476ee85ad02ddebf7018
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-07-28 22:36:52 -0400

    changed name to FirstName

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,7 +2,7 @@ import React from 'react';
 function Input(props) {
     const errorMessage = props.errors[props.name];
     return (
-        <label htmlFor="name">
+        <label htmlFor="FirstName">
             {props.label}
             <input {...props} />
             {errorMessage.length !== 0 && <p className="error">{errorMessage}</p>}
