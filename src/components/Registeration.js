918ff77455755cc4706d81e615195869a9ffaf10
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-07-29 20:35:19 -0400

    added axios

--- src/components/Registeration.js
+++ src/components/Registeration.js
@@ -2,91 +2,125 @@ import React, { useState, useEffect } from "react";
 import Input from "./Input";
 import axios from "axios";
 import * as yup from "yup";
+import { useHistory } from "react-router-dom";
+
+
 const Registeration = props => {
+    //** REACT 2 */
+    let history = useHistory();
+    //*** END REACT 2 */
+
     //Initial state
     const defaultState = {
+        firstName: "",
+        lastName: "",
         username: "",
         email: "",
         password: "",
         retype_password: "",
-        UserType: "seller",
-        Terms: false
+        type: "seller",
+        terms: false
     }
     // Form Schema 
     const FormSchema = yup.object().shape({
-        UserType: yup.string().notRequired(),
-        username: yup.string().required("Please Enter Your Name").min(2, "This is not your real name"),
-        email: yup.string().email().required("Please Enter email"),
+        type: yup.string().notRequired(),
+        firstName: yup.string().required("Please Enter Your first Name").min(2, "This is not your real name"),
+        lastName: yup.string().required("please enter LAstname"),
+        username: yup.string().required("Please Enter Your Name").min(8, "This is not your real name"),
+        email:yup.string().email().required("Please Enter email"),
         password: yup.string().required("Please enter a password").matches(
             /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
-            "Password must contain at least 8 characters, one uppercase, one number and one special case character"
-          ),
-        retype_password: yup
-            .string()
-            .required("Please confirm your password")
-            .when("Password", {
-                is: password => (FormState.password === FormState.password ? true : false),
-                then: yup.string().oneOf([yup.ref("Password")], "Password doesn't match")
-            }),
+            "Password must contain at least 8 characters, one uppercase, one lowercase,one number and one special case character"
+        ),
+        retype_password:yup
+                    .string()
+                    .required("Please confirm your password")
+                    .when("password", {
+                        is: password => (FormState.password===FormState.retype_password ? true : false),
+                        then: yup.string().oneOf([yup.ref("password")],"Password doesn't match")
+                    }),
         terms: yup.boolean().oneOf([true], 'please accept out terms')
     })
     const [FormState, SetFormState] = useState(defaultState);
     const [Error, Seterror] = useState({ ...defaultState, terms: "" });
-    const [Disablebutton, SetDisablebutton] = useState(true); const inputChange = event => {
+    const [Disablebutton, SetDisablebutton] = useState(true);
+
+
+    const inputChange = event => {
+       
         if (event.target.type === 'checkbox') {
-            console.log("checkbox", event.target.checked);
+
+            console.log("checkbox",event.target.checked);
             SetFormState({
                 ...FormState, [event.target.name]: event.target.checked
+
             })
         } else {
             SetFormState({
                 ...FormState,
                 [event.target.name]: event.target.value
             })
-        } const value = event.target.type === "checkbox" ? event.target.checked : event.target.value; event.persist(); yup.reach(FormSchema, event.target.name)
+        }
+            
+        const value = event.target.type==="checkbox" ? event.target.checked : event.target.value;
+       
+        event.persist();
+
+        yup.reach(FormSchema, event.target.name)
             .validate(value)
             .then(
                 valid => {
                     Seterror({ ...Error, [event.target.name]: "" })
-                })
+                }
+
+            )
             .catch(
                 err => {
                     console.log("Previous", err.errors[0]);
                     //  console.log("Error",err.Error);
                     Seterror({ ...Error, [event.target.name]: err.errors[0] })
-                })
-        //      const handleChange = e => {        //  }        console.log("Previous State", FormState);
+                }
+
+            )
+        //      const handleChange = e => {
+
+
+
+        //  }
+
+        console.log("Previous State", FormState);
     }
     useEffect(() => {
         FormSchema.isValid(FormState)
-            .then(valid =>
-            //       console.log("useeffect valid",valid)
-            //   SetDisablebutton(!valid));
-            {
-                if (FormState.username && FormState.firstName && FormState.lastName && FormState.password ) {
-                    SetDisablebutton(false);
-                }
-                else if (FormState.username || FormState.firstName || FormState.lastName || FormState.password ) {
-                    SetDisablebutton(true);
-                }
-            }
-            )
-    }, [FormState, FormSchema]) 
-       console.log("After", FormState); const SubmitForm = event => {
+        .then(valid => SetDisablebutton(!valid));
+    }, [FormState, FormSchema])
+
+    console.log("After", FormState);
+
+    const SubmitForm = event => {
         event.preventDefault();
         console.log("Formdata", FormState);
         axios.post("https://silent-auction-kb.herokuapp.com/api/auth/register", FormState)
-            .then(() => console.log('Form Submitted'))
-            .catch(err => console.log('There was a error in form', err));
+        .then(res => {
+            console.log(res);
+            history.push("/login");
+          })
+          .catch(err => {
+              console.log("invalid login.", err);
+          })    
     }
+
     return (
         <div className="formContainer">
-            <form onSubmit={SubmitForm}>                <div className="RadioContainer">
-                <label>Seller</label>
-                <input defaultChecked="Seller" type='radio' name='type' onChange={inputChange} data-cy='Seller' value='Seller' />  
-                                  <label> Bidder</label>
-                <input type='radio' name='type' onChange={inputChange} data-cy='Bidder' value='Bidder' />
-            </div>
+            <form onSubmit={SubmitForm}>
+
+                <div className="RadioContainer">
+                    <label>Seller</label>
+                    <input defaultChecked="Seller" type='radio' name='type' onChange={inputChange} data-cy='Seller' value='Seller' />
+
+                    <label> Bidder</label>
+                    <input type='radio' name='type' onChange={inputChange} data-cy='Bidder' value='Bidder' />
+                </div>
                 <Input
                     type="text"
                     name="firstName"
@@ -97,7 +131,7 @@ const Registeration = props => {
                 />
                 <Input
                     type="text"
-                    name="username"
+                    name="lastName"
                     onChange={inputChange}
                     value={FormState.lastName}
                     label="Last Name"
@@ -112,7 +146,7 @@ const Registeration = props => {
                     errors={Error}
                 />
                 <Input
-                    type="email"
+                    type="Email"
                     name="email"
                     onChange={inputChange}
                     value={FormState.email}
@@ -124,7 +158,7 @@ const Registeration = props => {
                     name="password"
                     onChange={inputChange}
                     value={FormState.password}
-                    label="password"
+                    label="Password"
                     errors={Error}
                 />
                 <Input
@@ -135,10 +169,11 @@ const Registeration = props => {
                     label="Retype Password"
                     errors={Error}
                 />
-                {/*<div className="TermsContainer">
+
+                <div className="TermsContainer">
                     <input type="checkbox" name="terms" onChange={inputChange} checked={FormState.terms} errors={Error} />
                     <label>Please Accept our Terms and conditions</label>
-            </div>*/}
+                </div>
                 <button disabled={Disablebutton}>Submit</button>
             </form>
         </div>
